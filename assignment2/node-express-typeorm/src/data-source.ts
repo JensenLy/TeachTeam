d995e9bae896cbd435a43d9a557944fd1084cf3b
JensenLy <s3977367@student.rmit.edu.au>
2025-06-02 22:47:36 +1000

    a start to the backend

---
+++ assignment2/node-express-typeorm/src/data-source.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "209.38.26.237",
+  port: 3306,
+  username: "S3977367",
+  password: "S3977367",
+  database: "S3977367",
+  // synchronize: true will automatically create database tables based on entity definitions
+  // and update them when entity definitions change. This is useful during development
+  // but should be disabled in production to prevent accidental data loss.
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
